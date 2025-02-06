88f694e35eb76d0bd2b5f96893a540237969b0c2
javadev19882025 <bhattjaimin350@gmail.com>
2025-02-06 22:36:17 +0530

    Simple Spring Boot Project File
    1) Create Get Request with Request Mapping
    2) Add JSP Pages
    3) Application Properties set prefix and Suffix

---
+++ starterbootwebapplication/src/main/java/com/example/starterboot/StarterbootwebapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.starterboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterbootwebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterbootwebapplicationApplication.class, args);
+	}
+
+}
