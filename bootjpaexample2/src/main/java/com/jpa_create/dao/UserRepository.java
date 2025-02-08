6c3aa1c979136ca05395c4d6dd4358d01fddc94d
javadev19882025 <bhattjaimin350@gmail.com>
2025-02-08 21:27:44 +0530

    Delete bootjpaexample2 directory

--- bootjpaexample2/src/main/java/com/jpa_create/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jpa_create.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.jpa_create.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
